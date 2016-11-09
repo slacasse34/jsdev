739658bb3da53a118b7f2b9c1ca1a2d4028137ca
slacasse34 <slacasse34@hotmail.com>
2016-11-09 15:51:54 -0500

    Added db stuff and using DirEntry add as 2 steps thing

--- FileInv/ScanMain.js
+++ FileInv/ScanMain.js
@@ -6,20 +6,47 @@
 var DirList = [];
 var FileList = [];
 var fs = require('fs');
+var mysql = require('mysql');   // npm install mysql
+
+var connection = mysql.createConnection({
+	host: '192.168.72.130',
+	user: 'sl',
+	password: '',
+	database: 'FileInv'
+});
+
+connection.connect(function(err){
+	if(err) throw err;
+	connection.query(
+		{
+			sql: 'select * from FileInfo'
+		},
+		function(err, rows, fields) {
+			if(err) throw err;
+			console.log('Input!');
+			for(var a=0; a<rows.length; a++){
+				console.log(rows[a]);
+			}
+			connection.end();
+		}
+	)
+});
 
 function IgnoreDir(Dir) {
 	return false;
 }
 
-function ScanDirTo(FromDir, OnDir, OnFile) {
-	var RootDir = fs.statSync(FromDir);
+function ScanDirTo(FromDir, NewDir, OnDir, OnFile) {
+	var DirStat = fs.statSync(FromDir);
 	var CurPath = FromDir;
 	var CurItem;
 	var i;
-	var Tot=0;
-	var FileSize=0;
+	var Tot = 0;
+	var FileSize = 0;
 	var CurDir = fs.readdirSync(FromDir);
+	var CurEntry;
 
+	Parent=NewDir(FromDir, DirStat);
 	for (i = 0; i < CurDir.length; i++) {
 		// CurItem=String(CurPath).endswith('\\') ? CurPath+'\\'+CurDir[i] : CurPath+'\\'+CurDir[i];
 		CurItem = CurPath + '\\' + CurDir[i];
@@ -28,27 +55,41 @@ function ScanDirTo(FromDir, OnDir, OnFile) {
 			if (IgnoreDir(CurItem))
 				continue;
 			DirList.push(CurItem);
-			Tot += ScanDirTo(CurItem, OnDir, OnFile);
+			Tot += ScanDirTo(CurItem, NewDir, OnDir, OnFile);
 			OnDir(CurItem, CurEntry, Tot);
 		}
 		else {
 			FileList.push(CurEntry);
-			FileSize += CurEntry.size>>10;
+			FileSize += CurEntry.size >> 10;
 			OnFile(CurItem, CurEntry);
 		}
 	}
-	OnDir(FromDir, RootDir, Tot+FileSize);
-	return Tot+FileSize;
+	OnDir(FromDir, DirStat, Tot + FileSize);
+	return Tot + FileSize;
 }
 
-function ScanDiskTo(FromDir, OnDir, OnFile) {
+function ScanDiskTo(FromDir, NewDir, OnDir, OnFile) {
 	var Tot;
 
 	DirList.push(FromDir);
-	Tot = ScanDirTo(FromDir, OnDir, OnFile);
+	Tot = ScanDirTo(FromDir, NewDir, OnDir, OnFile);
 	return Tot;
 }
 
+function NewDirEntryToDb(Filename, DirEntry) {
+	var rv=0;
+	console.log(Filename);
+	return rv;
+}
+
+function UpdateDbDirEntry(Filename, DirEntry, Kb) {
+	console.log(Filename);
+	console.log(DirEntry.size, Kb);
+}
+function FileEntryToDb(Filename, DirEntry, Parent) {
+	console.log(Filename);
+	console.log(DirEntry.size);
+}
 
 
 /* Debug */
@@ -62,7 +103,6 @@ function FileEntryToConsole(Filename, DirEntry) {
 	console.log(DirEntry.size);
 }
 
-
 /*
  * Main()
  */
@@ -71,7 +111,7 @@ if (process.argv.length != 3) {
 	return;
 }
 
-var KbSize=ScanDiskTo(process.argv[2], DirEntryToConsole, FileEntryToConsole);
-console.log(KbSize+' Kb');
+var KbSize = ScanDiskTo(process.argv[2], NewDirEntryToDb, UpdateDbDirEntry, FileEntryToDb);
+console.log(KbSize + ' Kb');
 console.log('Filelist:' + FileList.length);
 console.log('Dirlist:' + DirList.length);
